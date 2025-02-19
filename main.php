2d68d6e6a565cda0e3982049359fe9649dbc8a44
mijakub <kuba.mura@wp.pl>
2025-02-19 15:56:40 +0100

    Podstawy

---
+++ main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zarezerwuj lot!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        include_once("header.php");
+    ?>
+</body>
+</html>
