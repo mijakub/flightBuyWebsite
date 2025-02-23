9f8b0bfdaf3bc5279ddb673daf0e3edf6a91a25a
mijakub <kuba.mura@wp.pl>
2025-02-23 16:53:22 +0100

    Podstawy witryny

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
 <header>
-    
+    <h1><span class="icon">&#9992;</span> Zarezerwuj lot!</h1>
+    <div class="rightPanel">
+        <h3><a href="">Zaloguj się <span class="icon">&#128100;</span></a></h3>
+        <h3><a href="">Koszyk <span class="icon">&#128722;</span></a></h3>
+    </div>
 </header>
--- main.php
+++ main.php
@@ -5,10 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zarezerwuj lot!</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="planeIcon.png" type="image/x-icon">
 </head>
 <body>
+    <a name="top"></a>
+    <a href="#top"><img src="planeIcon.png" alt="back to the top icon" id="back"></a>
     <?php
         include_once("header.php");
     ?>
+    <nav>
+        <form action="main.php" method="POST" id="flightForm">
+            <div class="field">
+                <label id="from">Skąd:&nbsp;</label>
+                <select name="from" id="from">
+                    <option value="KTW">KTW</option>
+                    <option value="WAW">WAW</option>
+                    <option value="DXB">DXB</option>
+                </select>
+            </div>
+            <div class="field">
+                <label id="to">Dokąd:&nbsp;</label>
+                <select name="to" id="to">
+                    <option value="KTW">KTW</option>
+                    <option value="WAW">WAW</option>
+                    <option value="DXB">DXB</option>
+                </select>
+            </div>
+            <div class="field">
+                <label id="since">Od:&nbsp;</label>
+                <input type="text" name="since" id="since" pattern="\d{4}-\d{2}-\d{2}" placeholder="rrrr-mm-dd" required>
+            </div>
+            <div class="field">
+                <label id="until">Do:&nbsp;</label>
+                <input type="text" name="until" id="until" pattern="\d{4}-\d{2}-\d{2}" placeholder="rrrr-mm-dd" required>
+            </div>
+            <div class="field">
+                <label id="people">Ilość pasażerów:&nbsp;</label>
+                <input type="number" name="people" id="people" required min="1">
+            </div>
+            <div class="field">
+                <input type="submit" name="find" value="Znajdź lot!" id="find">
+            </div>
+        </form>
+    </nav>
 </body>
 </html>
